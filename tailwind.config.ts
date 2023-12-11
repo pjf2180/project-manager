56915c97d0315c39511306ee885b551795001ac6
Paul Jimenez <pjf2180@me.com>
2023-12-11 15:58:01 -0800

    Extended tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,54 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'blue': {
+          'xx-light': '#91A4F9',
+          'x-light': '#6F8BEF',
+          'light': '#4D78E5',
+          'DEFAULT': '#1665D8',
+        },
+        'green': {
+          'xx-light': '#82E675',
+          'x-light': '#68D265',
+          'light': '#4EBE54',
+          'DEFAULT': '#34AA44',
+        },
+        'red': {
+          'xx-light': '#F59D8D',
+          'x-light': '#F3816D',
+          'light': '#F1654D',
+          'DEFAULT': 'E6492D'
+        },
+        'orange': {
+          'xx-light': '#FEEB8F',
+          'x-light': '#FCE06F',
+          'light': '#FAC64F',
+          'DEFAULT': '#F6AB2F'
+        },
+        'yellow': {
+          'xx-light': '#FCFFB5',
+          'x-light': '#FBEF95',
+          'light': '#FBDF75',
+          'DEFAULT': '#FACF55'
+        },
+        'text': {
+          'main': '#3E3F42',
+          'secondary': '#6B6C6F',
+          'gray': '#BCBCBC'
+        }
+      },
     },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ["Roboto Slab", "serif"],
+      body: ["Roboto", "sans-serif"],
+    },
+    spacing: {
+      '1': '10px',
+      '2': '20px',
+      '3': '30px',
+    }
   },
   plugins: [],
 }
