948a6550b45a6768278e1491fe1c601b3675707d
Paul Jimenez <pjf2180@me.com>
2024-01-27 13:42:31 -0800

    Refactored member dropdown to use clickOutside utility hook

--- app/ui/shared/inputs/dropdown.tsx
+++ app/ui/shared/inputs/dropdown.tsx
@@ -1,39 +1,28 @@
 'use client';
-import { useEffect, useRef, useState, ReactNode } from "react";
+import { useRef, useState, ReactNode, MouseEvent } from "react";
+import { useClickOutside } from "../hooks/useClickOutside";
 
 export type DropdownInputs = {
     options: string[];
     selections: string[];
-    renderSelection: (id: string) => ReactNode;
-    renderOption: (id: string) => ReactNode;
     onSelection: (selection: string) => void;
+    renderSelection?: (id: string) => ReactNode;
+    renderOption?: (id: string) => ReactNode;
 };
 
 export function Dropdown({ options, selections, renderSelection, renderOption, onSelection }: DropdownInputs) {
 
     const containerRef = useRef<HTMLDivElement | null>(null);
     const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
-
-    useEffect(() => {
-        function onClickHandler(event: globalThis.MouseEvent) {
-            const target = event.target as Node;
-            if (containerRef.current && !containerRef.current.contains(target)) {
-                // onClickOutside();
-                setDrawerOpen(false);
-            }
-        };
-
-        document.addEventListener('click', onClickHandler);
-
-        return () => {
-            document.removeEventListener('click', onClickHandler)
-        }
-    }, []);
+    useClickOutside(containerRef, () => {
+        setDrawerOpen(false);
+    });
 
     const handleInputClick = () => {
         setDrawerOpen(true);
     };
-    const handleOptionClick = (selectedLabel: string) => {
+    const handleOptionClick = (e: MouseEvent<HTMLOptionElement>, selectedLabel: string) => {
+        e.stopPropagation();
         onSelection(selectedLabel);
     };
 
@@ -50,11 +39,13 @@ export function Dropdown({ options, selections, renderSelection, renderOption, o
             onClick={handleInputClick}>
             <div className="flex gap-2 p-1">
                 {
-                    selections.map((o: string) => renderSelection(o))
+                    selections.map((o: string) => {
+                        return renderSelection ? renderSelection(o) : <div>{o}</div>
+                    })
                 }
             </div>
             {
-                drawerOpen && notSelectedOptions.length > 0 && <div
+                drawerOpen && notSelectedOptions.length >0 && <div
                     className="bg-white border rounded-md rounded-t-none border-t-0"
                     style={{ position: 'absolute', top: '50px', left: '-1px', right: '-1px', maxHeight: '120px' }}>
                     {
@@ -63,10 +54,10 @@ export function Dropdown({ options, selections, renderSelection, renderOption, o
                                 key={notSelectedOptionId}
                                 className="hover:bg-blue-100 p-2"
                                 style={{ cursor: 'pointer' }}
-                                onClick={() => handleOptionClick(notSelectedOptionId)}>
-                                    {
-                                        renderOption(notSelectedOptionId)
-                                    }
+                                onClick={(e) => handleOptionClick(e, notSelectedOptionId)}>
+                                {
+                                    renderOption? renderOption(notSelectedOptionId) : <div>{notSelectedOptionId}</div>
+                                }
                             </option>
                         })
                     }
