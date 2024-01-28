9f7de3dac82b322714ae9e679436407537bc4519
Paul Jimenez <pjf2180@me.com>
2024-01-27 19:57:17 -0800

    Fixed useClickOutside utility bug

--- app/ui/shared/hooks/useClickOutside.ts
+++ app/ui/shared/hooks/useClickOutside.ts
@@ -7,7 +7,7 @@ export function useClickOutside(elRef: MutableRefObject<HTMLDivElement | null>, 
     useEffect(() => {
         function onClickHandler(event: globalThis.MouseEvent) {
             const target = event.target as Node;
-            if (elRef.current && !elRef.current.contains(target)) {
+            if (elRef.current && document.contains(target) && !elRef.current.contains(target)) {
                 handler();
             }
         };
